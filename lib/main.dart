2999f86e08c6ee65e2d7fecf6c390287742cd694
patrick <patrickchow0803@gmail.com>
2020-02-08 11:41:45 -0500

    Added a container to hold a child text.

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.red,
-        body: Container(),
+        body: SafeArea(
+          child: Container(
+            width: 100.0,
+            height: 100.0,
+//            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello World'),
+          ),
+        ),
       ),
     );
   }
